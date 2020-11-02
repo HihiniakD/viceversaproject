c8662cf2a68411c7122d55b4de30eda1a35a85ad
DmythoH <dimon4iklimon4ik@ukr.net>
2020-11-02 21:32:43 +0200

    Add words count feature

--- viceversaproject/pages.py
+++ viceversaproject/pages.py
@@ -7,5 +7,6 @@ def form(request):
 
 def reverse(request):
 	user_text = request.GET['usertext']
+	number_of_words = len(user_text.split())
 	reversed_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text})
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 'numberofwords': number_of_words})
